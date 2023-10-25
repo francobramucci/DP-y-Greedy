a52bd29279493b58b40969e22d7f0149cdcb7522
francobramucci <bramuccifranco1@gmail.com>
2023-10-25 01:45:55 -0300

    Problema resuelto, queda la parte de backtracking y hacerlo más lindo

--- damas.cpp
+++ damas.cpp
@@ -7,30 +7,70 @@ using namespace std;
 #define forn(i,n) forr(i,0,n)
 #define endl '\n'
 #define pb push_back
+#define MAX 10000
 
 using ll = long long;
 using vi = vector<int>;
 using ii = pair<int,int>;
 
-
 int main(){
     int n;
     cout << "Ingrese el tamaño del tablero: "; cin >> n;
 
-    vector<vi> board(n);
-    vector<vi> sub(n);
+    int board[1000][1000] = {0};
+    int sub[1000][1000] = {0};
 
     forn(i,n){
-        forn(j,n){
+        forr(j,1,n+1){
             int w; cin >> w;
-            board[i].push_back(w);
+            board[i][j] = w;
             cout << board[i][j] << ' ';
         }
         cout << endl;
     }
 
+
     forn(i,n){
-        sub[0].push_back(board[0][i]);
+        forn(j,n+2){
+            sub[i][j] = MAX;
+        }
     }
 
+    forr(i, 1, n+1){
+        sub[0][i] = board[0][i];
+    }
+
+    // cout << endl << endl;
+    
+    // forn(i,n){
+    //     forr(j,1,n+1){
+    //         cout << sub[i][j] << ' ';
+    //     }
+    //     cout << endl;
+    // }
+
+
+    forr(i, 1, n){
+        forr(j,1,n+1){
+            sub[i][j] = board[i][j] + min(sub[i-1][j], min(sub[i-1][j+1], sub[i-1][j-1]));
+        }
+    }
+
+    cout << endl << endl;
+
+    forn(i,n){
+        forr(j,1,n+1){
+            cout << sub[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+
+    int minimo = MAX;
+
+    forr(j,1,n+1){
+        minimo = min(minimo, sub[n-1][j]);
+        
+    }
+    cout << "El costo del camino mas corto es: " << minimo << "!!" << endl;
 }
